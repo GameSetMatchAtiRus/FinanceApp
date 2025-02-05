a3605a6661489a4fbb93d01009ee8a3b8b0ffa89
GameSetMatchAtiRus <47218493+GameSetMatchAtiRus@users.noreply.github.com>
2025-02-05 14:54:51 +0300

    Add files via upload

---
+++ CatchXMLBankApp/CatchXMLBankApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+class Program
+{
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Введите код валюты (например, USD, EUR):");
+        string currencyCode = Console.ReadLine().ToUpper();
+
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        // Запрос котировок валют
+        string url = "https://www.cbr.ru/scripts/XML_daily.asp?date_req=" + DateTime.Today.ToString();
+        using HttpClient client = new HttpClient();
+
+        try
+        {
+            // Получение XML
+            var response = await client.GetStringAsync(url);
+            XDocument doc = XDocument.Parse(response.ToString());
+            var currency = doc.Descendants("Valute").FirstOrDefault(v => v.Element("CharCode")?.Value == currencyCode);
+
+            if (currency != null)
+            {
+                // Формирование ответа
+                string name = currency.Element("Name")?.Value;
+                string value = currency.Element("Value")?.Value;
+                Console.WriteLine($"На момент {DateTime.Now} котировка {name} ({currencyCode}): {value} рублей.");
+            }
+            else
+            {
+                Console.WriteLine("Валюта не найдена.");
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Ошибка при запросе данных: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Произошла ошибка: {e.Message}");
+        }
+    }
+}
---
+++ CatchXMLBankApp/FirstThreeTeamsPointsSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Массив очков, набранных каждой из 20 команд
+            int[] points = { 35, 42, 28, 50, 37, 41, 33, 29, 45, 38, 40, 36, 39, 44, 30, 27, 32, 34, 31, 43 };
+
+            // Сортируем массив очков в порядке убывания
+            int[] sortedPoints = points.OrderByDescending(p => p).ToArray();
+
+            // Суммируем очки первых трёх команд
+            int topThreeSum = sortedPoints[0] + sortedPoints[1] + sortedPoints[2];
+
+            // Выводим результат
+            Console.WriteLine($"Сумма очков первых трёх команд: {topThreeSum}");
+        }
+    }
+}
